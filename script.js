26a2da21a4ef7f6b313a1e0850d57b63a08a80f1
GiorgiJamaspishvili <126399819+GiorgiJamaspishvili@users.noreply.github.com>
2023-05-01 15:48:04 +0400

    array1

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// 1.
+// let x = 5;
+// while (x <= 100) {
+//     console.log(x);
+//     x++;
+// }
+
+// OR => ||
+
+// while (true) {
+//     console.log(x);
+//     x++;
+//     if (x == 101) {
+//         break;
+//     }
+// }
+
+
+
+
+// 2.
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let item of array1){
+//     if (item < 10 && item > 0){
+//         console.log(item);
+//     }
+// }
+
+
+
+// 3.
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let item of array2){
+//     if(array2[item] == 5)
+//     console.log('Aris');
+// }
+
+
+// 4.
+// let array3 = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < array3.length; i++){
+//     sum += array3[i];
+//     }
+// console.log(sum)
+
+
+// 5.
+// let array4 = [1, 2, 3, 7, 6, 9];
+// let sum = 0;
+// for (let i = 0; i < array4.length; i++){
+//     sum += array4[i] / array4.length
+// }
+// console.log(sum)
+
+
+// 6.
+
+
+
+
+
